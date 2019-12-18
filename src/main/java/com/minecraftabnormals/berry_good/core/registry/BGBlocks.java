7f36338a568cf1c85209e27d8c42f24deef4f57a
Cameron <eboysix@hotmail.co.nz>
2019-12-18 17:51:09 +1300

    Fixed the filepath

--- src/main/java/com/minecraftabnormals/berry_good/core/registry/BGBlocks.java
+++ src/main/java/com/minecraftabnormals/berry_good/core/registry/BGBlocks.java
@@ -6,7 +6,6 @@ import com.minecraftabnormals.berry_good.core.registry.util.RegistryUtils;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,5 +14,5 @@ public class BGBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, Berry_Good.MOD_ID);
     
-    public static final RegistryObject<Block> SWEET_BERRY_SEEDS = RegistryUtils.createBlockNoItem("sweet_berry_bush_seeds", () -> new BlockSweetBerryBush(Block.Properties.from(Blocks.SWEET_BERRY_BUSH)));
+    public static final RegistryObject<Block> SWEET_BERRY_BUSH_SEEDS = RegistryUtils.createBlockNoItem("sweet_berry_bush_seeds", () -> new BlockSweetBerryBush(Block.Properties.from(Blocks.SWEET_BERRY_BUSH)));
 }
