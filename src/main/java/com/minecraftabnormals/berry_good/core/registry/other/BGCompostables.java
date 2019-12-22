dcf11757d4bc872e40524bc1ef3b1c9f02beabc3
Cameron <eboysix@hotmail.co.nz>
2019-12-22 22:47:16 +1300

    Made sweet berries compostable again

--- src/main/java/com/minecraftabnormals/berry_good/core/registry/other/BGCompostables.java
+++ src/main/java/com/minecraftabnormals/berry_good/core/registry/other/BGCompostables.java
@@ -1,6 +1,7 @@
 package com.minecraftabnormals.berry_good.core.registry.other;
 
 import com.minecraftabnormals.berry_good.core.registry.BGItems;
+import com.minecraftabnormals.berry_good.core.registry.BGRegistryReplacements;
 
 import net.minecraft.block.ComposterBlock;
 
@@ -8,6 +9,7 @@ public class BGCompostables {
 
 	public static void registerCompostables() {
 		ComposterBlock.registerCompostable(0.3F, BGItems.SWEET_BERRY_PIPS.get());
+		ComposterBlock.registerCompostable(0.3F, BGRegistryReplacements.SWEET_BERRIES);
 	}
 	
 }
