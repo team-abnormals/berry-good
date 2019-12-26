f4bb63b567974ab293625ecc8f2aed4c5ae68516
Cameron <eboysix@hotmail.co.nz>
2019-12-26 17:48:10 +1300

    Fixed saturation level for meatballs

--- src/main/java/com/minecraftabnormals/berry_good/core/registry/BGFoods.java
+++ src/main/java/com/minecraftabnormals/berry_good/core/registry/BGFoods.java
@@ -7,6 +7,6 @@ import net.minecraft.potion.Effects;
 public class BGFoods {
 
 	public static final Food MEATBALLS(boolean cooked) {
-		return cooked ? new Food.Builder().hunger(10).saturation(12.8F).build() : new Food.Builder().hunger(5).saturation(0.6F).build();
+		return cooked ? new Food.Builder().hunger(10).saturation(1.28F).build() : new Food.Builder().hunger(5).saturation(0.6F).build();
 	}
 }
