bf8726828c8876e257fa09ec2b8ad509889d7333
five0five <44749261+five0five@users.noreply.github.com>
2020-01-27 02:13:12 +0000

    forgot raw

--- src/main/java/com/minecraftabnormals/berry_good/core/registry/BGFoods.java
+++ src/main/java/com/minecraftabnormals/berry_good/core/registry/BGFoods.java
@@ -7,6 +7,6 @@ import net.minecraft.potion.Effects;
 public class BGFoods {
 
 	public static final Food MEATBALLS(boolean cooked) {
-		return cooked ? new Food.Builder().hunger(10).saturation(0.7F).build() : new Food.Builder().hunger(5).saturation(0.4F).build();
+		return cooked ? new Food.Builder().hunger(10).saturation(0.7F).build() : new Food.Builder().hunger(5).saturation(0.2F).build();
 	}
 }
