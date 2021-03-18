33ffda280c599437d2e96a238b138896deb7aea2
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-03-18 18:55:16 -0500

    Updated Fox comparator value

--- src/main/java/com/minecraftabnormals/berry_good/core/registry/BGItems.java
+++ src/main/java/com/minecraftabnormals/berry_good/core/registry/BGItems.java
@@ -15,7 +15,7 @@ public class BGItems {
 	public static final RegistryObject<Item> SWEET_BERRY_MEATBALLS = HELPER.createItem("sweet_berry_meatballs", () -> new Item(new Item.Properties().group(ItemGroup.FOOD).food(Foods.SWEET_BERRY_MEATBALLS)));
 	public static final RegistryObject<Item> SWEET_BERRY_PIPS = HELPER.createItem("sweet_berry_pips", () -> new BlockNamedItem(BGBlocks.SWEET_BERRY_BUSH_PIPS.get(), new Item.Properties().group(ItemGroup.MISC)));
 
-	public static final RegistryObject<Item> MUSIC_DISC_FOX = HELPER.createItem("music_disc_fox", () -> new AbnormalsMusicDiscItem(17, BGSounds.FOX, new Item.Properties().maxStackSize(1).group(ItemGroup.MISC).rarity(Rarity.RARE)));
+	public static final RegistryObject<Item> MUSIC_DISC_FOX = HELPER.createItem("music_disc_fox", () -> new AbnormalsMusicDiscItem(2, BGSounds.FOX, new Item.Properties().maxStackSize(1).group(ItemGroup.MISC).rarity(Rarity.RARE)));
 
 	static class Foods {
 		public static final Food SWEET_BERRY_MINCE = new Food.Builder().hunger(5).saturation(0.2F).build();
