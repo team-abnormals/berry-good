1fd0b83d92568356dc1e2545fcc038c4ac7c2af9
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2022-03-30 14:03:19 -0500

    Update to Blueprint 5.3.0 for 1.18.2

--- src/main/java/com/teamabnormals/berry_good/core/data/server/modifiers/BGAdvancementModifiersProvider.java
+++ src/main/java/com/teamabnormals/berry_good/core/data/server/modifiers/BGAdvancementModifiersProvider.java
@@ -1,29 +1,18 @@
 package com.teamabnormals.berry_good.core.data.server.modifiers;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.mojang.datafixers.util.Pair;
 import com.teamabnormals.berry_good.core.BerryGood;
 import com.teamabnormals.berry_good.core.registry.BGBlocks;
 import com.teamabnormals.berry_good.core.registry.BGItems;
-import com.teamabnormals.blueprint.common.advancement.modification.modifiers.AdvancementModifier;
-import com.teamabnormals.blueprint.common.advancement.modification.modifiers.AdvancementModifier.Mode;
+import com.teamabnormals.blueprint.common.advancement.modification.AdvancementModifierProvider;
 import com.teamabnormals.blueprint.common.advancement.modification.modifiers.CriteriaModifier;
-import com.teamabnormals.blueprint.common.advancement.modification.modifiers.IndexedRequirementsModifier;
-import com.teamabnormals.blueprint.core.util.modification.AdvancementModifierProvider;
-import net.minecraft.advancements.Criterion;
+import net.minecraft.advancements.RequirementsStrategy;
 import net.minecraft.advancements.critereon.ConsumeItemTrigger;
-import net.minecraft.advancements.critereon.PlacedBlockTrigger.TriggerInstance;
+import net.minecraft.advancements.critereon.PlacedBlockTrigger;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.Collection;
 
 public class BGAdvancementModifiersProvider extends AdvancementModifierProvider {
 
@@ -33,38 +22,17 @@ public class BGAdvancementModifiersProvider extends AdvancementModifierProvider 
 
 	@Override
 	protected void registerEntries() {
-		this.registerEntry("husbandry/balanced_diet", createCriteriaModifier(Mode.MODIFY, consumeItem(BGItems.SWEET_BERRY_MINCE.get()), consumeItem(BGItems.SWEET_BERRY_MEATBALLS.get())), new ResourceLocation("husbandry/balanced_diet"));
-		this.registerEntry("husbandry/plant_seed", createIndexedRequirementsModifier(0, Collections.singletonList(Pair.of("sweet_berry_bush_pips", new Criterion(TriggerInstance.placedBlock(BGBlocks.SWEET_BERRY_BUSH_PIPS.get()))))), new ResourceLocation("husbandry/plant_seed"));
-	}
-
-	private static Pair<String, Criterion> consumeItem(ItemLike item) {
-		return Pair.of(item.asItem().getRegistryName().getPath(), new Criterion(ConsumeItemTrigger.TriggerInstance.usedItem(item.asItem())));
-	}
-
-	private Pair<Optional<Map<String, Criterion>>, Optional<String[]>> collectCriterions(List<Pair<String, Criterion>> criterions) {
-		Optional<Map<String, Criterion>> criterionMap = Optional.of(Maps.newHashMap());
-		ArrayList<String> reqs = com.google.common.collect.Lists.newArrayList();
-
-		criterions.forEach(pair -> {
-			criterionMap.get().put(this.getModId() + ":" + pair.getFirst(), pair.getSecond());
-			reqs.add(this.getModId() + ":" + pair.getFirst());
+		CriteriaModifier.Builder balancedDiet = CriteriaModifier.builder(this.modId);
+		Collection<RegistryObject<Item>> items = BGItems.HELPER.getDeferredRegister().getEntries();
+		items.forEach(item -> {
+			if (item.get().isEdible()) {
+				balancedDiet.addCriterion(item.get().getRegistryName().getPath(), ConsumeItemTrigger.TriggerInstance.usedItem(item.get()));
+			}
 		});
-
-		return Pair.of(criterionMap, Optional.of(reqs.toArray(String[]::new)));
-	}
-
-	private CriteriaModifier createCriteriaModifier(Mode mode, Pair<String, Criterion>... criterions) {
-		Pair<Optional<Map<String, Criterion>>, Optional<String[]>> requirements = collectCriterions(Arrays.asList(criterions));
-		ArrayList<String[]> reqs = Lists.newArrayList();
-		for (String string : requirements.getSecond().get()) {
-			reqs.add(new String[]{string});
-		}
-
-		return new CriteriaModifier(mode, requirements.getFirst(), Optional.of(reqs.toArray(String[][]::new)));
-	}
-
-	private IndexedRequirementsModifier createIndexedRequirementsModifier(int index, List<Pair<String, Criterion>> criterions) {
-		Pair<Optional<Map<String, Criterion>>, Optional<String[]>> requirements = collectCriterions(criterions);
-		return new IndexedRequirementsModifier(AdvancementModifier.Mode.MODIFY, index, requirements.getFirst(), requirements.getSecond());
+		this.entry("husbandry/balanced_diet").selects("husbandry/balanced_diet").addModifier(balancedDiet.requirements(RequirementsStrategy.AND).build());
+		this.entry("husbandry/plant_seed").selects("husbandry/plant_seed").addModifier(CriteriaModifier.builder(this.modId)
+				.addCriterion("sweet_berry_bush_pips", PlacedBlockTrigger.TriggerInstance.placedBlock(BGBlocks.SWEET_BERRY_BUSH_PIPS.get()))
+				.addCriterion("cave_vine_pips", PlacedBlockTrigger.TriggerInstance.placedBlock(BGBlocks.CAVE_VINE_PIPS.get()))
+				.addIndexedRequirements(0, false, "sweet_berry_bush_pips", "cave_vine_pips").build());
 	}
 }
