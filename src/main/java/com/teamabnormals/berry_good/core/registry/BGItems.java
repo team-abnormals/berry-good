37091031847216f7b81082c81cd501a8062f92ec
Kitteh6660 <Kitteh6660@users.noreply.github.com>
2023-07-22 07:55:35 -0400

    Creative mode tabs for items

--- src/main/java/com/teamabnormals/berry_good/core/registry/BGItems.java
+++ src/main/java/com/teamabnormals/berry_good/core/registry/BGItems.java
@@ -8,9 +8,16 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BowlFoodItem;
+import net.minecraft.world.item.CreativeModeTab.TabVisibility;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.util.MutableHashedLinkedMap;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.registries.RegistryObject;
 
@@ -31,5 +38,24 @@ public class BGItems {
 		public static final FoodProperties SWEET_BERRY_MINCE = new FoodProperties.Builder().nutrition(5).saturationMod(0.3F).build();
 		public static final FoodProperties SWEET_BERRY_MEATBALLS = new FoodProperties.Builder().nutrition(10).saturationMod(0.8F).build();
 		public static final FoodProperties GLOWGURT = new FoodProperties.Builder().nutrition(10).saturationMod(0.6F).effect(() -> new MobEffectInstance(MobEffects.GLOWING, 3000), 1.0F).build();
+	}
+	
+	@SubscribeEvent
+	public static void addItemsToTabs(BuildCreativeModeTabContentsEvent event) {
+		MutableHashedLinkedMap<ItemStack, TabVisibility> map = event.getEntries();
+		if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+			map.putAfter(Items.SWEET_BERRIES.getDefaultInstance(), SWEET_BERRY_PIPS.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+			map.putAfter(Items.GLOW_BERRIES.getDefaultInstance(), GLOW_BERRY_PIPS.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+			map.remove(Items.SWEET_BERRIES.getDefaultInstance());
+			map.remove(Items.GLOW_BERRIES.getDefaultInstance());
+		}
+		if (event.getTabKey() == CreativeModeTabs.FOOD_AND_DRINKS) {
+			map.putAfter(Items.SWEET_BERRIES.getDefaultInstance(), SWEET_BERRY_MINCE.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+			map.putAfter(SWEET_BERRY_MINCE.get().getDefaultInstance(), SWEET_BERRY_MEATBALLS.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+			map.putAfter(Items.GLOW_BERRIES.getDefaultInstance(), GLOWGURT.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+		}
+		if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+			map.putBefore(Items.MUSIC_DISC_5.getDefaultInstance(), MUSIC_DISC_FOX.get().getDefaultInstance(), TabVisibility.PARENT_AND_SEARCH_TABS);
+		}
 	}
 }
